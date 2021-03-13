1d0e0529425e9ae84233c67cd9a444933a92a756
danny00747 <belinemugisha21@gmail.com>
2021-03-13 21:58:22 +0100

    added init files

---
+++ src/app/github.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import {User} from './user';
+import {Repos} from './repos';
+import {environment} from '../environments/environment';
+
+
+
+@Injectable()
+export class GithubService {
+  username: string;
+
+  constructor(private _http: HttpClient) {
+    this.username =  'Mugisha-Beline';
+
+  }
+  getUser() {
+    return this._http.get('https://api.github.com/users/' + this.username)
+    .map(result => result);
+
+  }
+  getRepos() {
+    return this._http.get(' https://api.github.com/users/' + this.username + '/repos')
+    .map(result => result);
+}
+updateUser(username: string) {
+  this.username = username;
+}
+}
